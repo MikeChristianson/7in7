295f08d326c76f46df1768a2dafba5f2543f262f
Mike Christianson <mike@codeaweso.me>
2012-09-16 23:00:22 -0700

    exercies from day 1, authors and books

---
+++ Prolog/Day-1/authors_and_books.pl
@@ -0,0 +1,7 @@
+author(stephen_king, the_gunslinger).
+author(bob_martin, clean_code).
+author(ayn_rand, atlas_shrugged).
+author(david_allen, getting_things_done).
+author(stephen_king, the_eyes_of_the_dragon).
+
+author_of(X, Y) :- author(X, Y).
