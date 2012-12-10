a9dbfbee68e819a840398355ab06f8694bede380
Mike Christianson <mike@codeaweso.me>
2012-12-10 03:15:58 -0700

    took me a while, but I finally got a working implementation of a word counter.
    do I get bonus points for not using built-ins?

--- Erlang/Day-1/counter.erl
+++ Erlang/Day-1/counter.erl
@@ -1,6 +1,13 @@
 -module (counter).
 -export ([count_char/1]).
+-export ([count_word/1]).
 
 count_char(String) -> count_char(String, 0).
 count_char([Head | Tail], Accumulator) -> count_char(Tail, Accumulator + 1);
 count_char([], Accumulator) -> Accumulator.
+
+count_word(String) -> count_word(String, 0, "NotInWord").
+count_word([32 | Tail], Accumulator, _) -> count_word(Tail, Accumulator, "NotInWord");
+count_word([Head | Tail], Accumulator, "NotInWord") -> count_word(Tail, Accumulator + 1, "InWord");
+count_word([Head | Tail], Accumulator, "InWord") -> count_word(Tail, Accumulator, "InWord");
+count_word([], Accumulator, _) -> Accumulator.
