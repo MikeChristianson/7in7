239f4e286f70b01315034d42343e86a56d6fd7d7
Mike Christianson <mike@codeaweso.me>
2012-12-09 19:07:21 -0700

    my implementation of a truly tail-recursive factorial

--- Erlang/Day-1/yet_again.erl
+++ Erlang/Day-1/yet_again.erl
@@ -2,8 +2,9 @@
 -export ([another_factorial/1]).
 -export ([another_fib/1]).
 
-another_factorial(0) -> 1;
-another_factorial(N) -> N * another_factorial(N-1).
+another_factorial(N) -> another_factorial(N, 1).
+another_factorial(1, Accumulator) -> Accumulator;
+another_factorial(N, Accumulator) -> another_factorial(N - 1, Accumulator * N).
 
 another_fib(0) -> 1;
 another_fib(1) -> 1;
